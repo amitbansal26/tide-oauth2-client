8bcaa026141bb0692d1da4968a1fa8e2c6f7f59e
Amit Bansal <amitbansal26@gmail.com>
2020-11-29 17:25:24 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use actix_session::{CookieSession, Session};
+use actix_web::http::header;
+use actix_web::web;
 use actix_web::{App, HttpResponse, HttpServer};
 use oauth2::basic::BasicClient;
-use oauth2::{CsrfToken, PkceCodeChallenge, Scope};
-
+use oauth2::{
+    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl,
+    Scope, TokenUrl,
+};
+use serde::Deserialize;
 struct AppState {
     oauth: BasicClient,
 }
@@ -26,12 +31,96 @@ fn index(session: Session) -> HttpResponse {
 
     HttpResponse::Ok().body(html)
 }
+
 #[actix_rt::main]
 async fn main() {
-    HttpServer::new(|| App::new().route("/", actix_web::web::get().to(index)))
-        .bind("127.0.0.1:5000")
-        .expect("Can not bind to port 5000")
-        .run()
-        .await
-        .unwrap();
+    HttpServer::new(|| {
+        let keycloak_client_id = ClientId::new(String::from("rust-microservice"));
+        let google_client_secret =
+            ClientSecret::new(String::from("d3b0fbf3-7d0d-4d99-bd1c-697febc6147d"));
+        let auth_url = AuthUrl::new(
+            "http://localhost:8080/auth/realms/dev/protocol/openid-connect/auth".to_string(),
+        )
+        .expect("Invalid authorization endpoint URL");
+        let token_url = TokenUrl::new(
+            "http://localhost:8080/auth/realms/dev/protocol/openid-connect/token".to_string(),
+        )
+        .expect("Invalid token endpoint URL");
+
+        // Set up the config for the Google OAuth2 process.
+        let client = BasicClient::new(
+            keycloak_client_id,
+            Some(google_client_secret),
+            auth_url,
+            Some(token_url),
+        );
+
+        App::new()
+            .data(AppState { oauth: client })
+            .wrap(CookieSession::signed(&[0; 32]).secure(false))
+            .route("/", web::get().to(index))
+            .route("/login", web::get().to(login))
+            .route("/logout", web::get().to(logout))
+            .route("/auth", web::get().to(auth))
+    })
+    .bind("127.0.0.1:8085")
+    .expect("Can not bind to port 8085")
+    .run()
+    .await
+    .unwrap();
+}
+
+fn login(data: web::Data<AppState>) -> HttpResponse {
+    let (pkce_code_challange, _pkce_code_verifier) = PkceCodeChallenge::new_random_sha256();
+
+    let (authorize_url, _csrf_state) = &data
+        .oauth
+        .authorize_url(CsrfToken::new_random)
+        .set_pkce_challenge(pkce_code_challange)
+        .url();
+
+    HttpResponse::Found()
+        .header(header::LOCATION, authorize_url.to_string())
+        .finish()
+}
+fn logout(session: Session) -> HttpResponse {
+    session.remove("login");
+    HttpResponse::Found()
+        .header(header::LOCATION, "/".to_string())
+        .finish()
+}
+#[derive(Deserialize)]
+pub struct AuthRequest {
+    code: String,
+    state: String,
+    scope: String,
+}
+fn auth(
+    session: Session,
+    data: web::Data<AppState>,
+    params: web::Query<AuthRequest>,
+) -> HttpResponse {
+    let code = AuthorizationCode::new(params.code.clone());
+    let state = CsrfToken::new(params.state.clone());
+    let _scope = params.scope.clone();
+
+    // Exchange the code with a token.
+    let token = &data.oauth.exchange_code(code);
+    println!("Access Token {:?}", token);
+    session.set("login", true).unwrap();
+
+    let html = format!(
+        r#"<html>
+        <head><title>OAuth2 Test</title></head>
+        <body>
+            Keycloak returned the following state:
+            <pre>{}</pre>
+            Keycloak returned the following token:
+            <pre>{:?}</pre>
+        </body>
+    </html>"#,
+        state.secret(),
+        token
+    );
+    HttpResponse::Ok().body(html)
 }
